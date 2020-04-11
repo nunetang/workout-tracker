7d6949d41466252d7c7f0bc9e6188e507f7de81f
Nune Tangamyan <38163311+nunetang@users.noreply.github.com>
2020-04-11 11:32:01 -0700

    changes in server.js for heroku deployment

--- Develop/server.js
+++ Develop/server.js
@@ -1,31 +1,23 @@
-// Dependencies
 const express = require("express");
-const morgan = require("morgan");
+const logger = require("morgan");
 const mongoose = require("mongoose");
 
-// Setting up Express App
+const PORT = process.env.PORT || 3000;
+
 const app = express();
-const PORT = process.env.PORT || 8000;
 
-app.use(morgan("dev"));
-
-// Sets up the Express app to handle data parsing
+app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-// db mongo
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
-mongoose.connect(MONGODB_URI, {
-    useNewUrlParser: true,
-    useFindAndModify: false
-})
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
-// Creating Routes
+const db = require("./models");
+
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
-// Starts the server to begin listening
-app.listen(PORT, function(){
-    console.log(`App listening on Port ${PORT}!`);
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
 });
